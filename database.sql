7cedec4a65d49921f9c4a838845a527f3f2908e0
gamid <kalldrek@yandex.ru>
2023-12-07 19:37:36 +0300

    last commit

---
+++ database.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Urls (
+    id SERIAL PRIMARY KEY,
+    name varchar(255),
+    created_at date
+
+);
+
+CREATE TABLE Url_Checks (
+    id SERIAL PRIMARY KEY,
+    url_id bigint REFERENCES urls (id),
+    status_code integer,
+    h1 varchar(255),
+    title varchar(255),
+    description text,
+    created_at date
+);
