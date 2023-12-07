5e562e415a2625c2da705bde3450fe54407a8f7f
gamid <kalldrek@yandex.ru>
2023-12-07 20:01:43 +0300

    last commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from sqlalchemy import desc
 from flask import Flask, redirect, render_template, request, url_for, flash
 import os
 # from validator import validate
-from page_analyzer import validator
+import validator
 from flask_sqlalchemy import SQLAlchemy
 import requests
 from bs4 import BeautifulSoup
