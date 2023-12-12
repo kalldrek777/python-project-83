1723e6ab7067604f22fa34e4c5c111d9da84f7b5
gamid <kalldrek@yandex.ru>
2023-12-12 18:46:34 +0300

    f

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -78,7 +78,7 @@ def new_url():
     urls = Urls.query.order_by('created_at').all()
     for url in urls:
         if data_url == url.name:
-            flash('Страница уже существует')
+            flash('Страница успешно добавлена')
             return redirect(url_for('url_page', id=url.id))
 
     db.session.add(url_id)
